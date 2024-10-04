05687856433bdd295c75c96ce75f3565af7a6758
Daniel Ho <daniel.ho337@gmail.com>
2024-10-04 16:21:39 -0700

    Created an anime seed file

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,6 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .animeSeed import seed_anime, undo_anime
 
 from app.models.db import db, environment, SCHEMA
 
@@ -16,13 +17,16 @@ def seed():
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
+        undo_anime()
         undo_users()
     seed_users()
+    seed_anime()
     # Add other seed functions here
 
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
+    undo_anime()
     undo_users()
     # Add other undo functions here
