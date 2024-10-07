d7396293878ef2da11bbfb9f1dbbd71172215b4e
Daniel Ho <daniel.ho337@gmail.com>
2024-10-07 07:39:08 -0700

    Completed the episode form.
    Decide to have the preview image be mandatory for both anime and episode section for now.

--- app/forms/anime_form.py
+++ app/forms/anime_form.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileAllowed, FileField, FileRequired
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from app.api.s3_helper import ALLOWED_EXTENSIONS
@@ -7,5 +7,5 @@ from app.api.s3_helper import ALLOWED_EXTENSIONS
 class AnimeForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
     synopsis = StringField('Synopsis', validators=[DataRequired()])
-    previewImage = FileField('Preview Image', validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
+    previewImage = FileField('Preview Image', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
     submit = SubmitField("Post Anime")
--- app/forms/episode_form.py
+++ app/forms/episode_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
-from wtforms import DateField, IntegerField, StringField
+from flask_wtf.file import FileAllowed, FileField, FileRequired
+from wtforms import DateField, IntegerField, StringField, SubmitField
 from wtforms.validators import DataRequired
 from app.api.s3_helper import ALLOWED_EXTENSIONS
 
@@ -10,3 +10,5 @@ class EpisodeForm(FlaskForm):
     plot = StringField('Plot', validators=[DataRequired()])
     order = IntegerField('Episode #', validators=[DataRequired()])
     airDate = DateField('Aired Date', validators=[DataRequired()])
+    previewImage = FileField('Preview Image', validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+    submit = SubmitField('Add Episode')
