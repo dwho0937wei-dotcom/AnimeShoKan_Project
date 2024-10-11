d4ee2f50e6342f73b567244bd4b2235836b868b0
Daniel Ho <daniel.ho337@gmail.com>
2024-10-11 12:16:10 -0700

    Have the episode be listed in flex-direction column in the AnimePage. Have each episode be 20px from each other.
    Added a nonfunctional delete button for EpisodePage, and the "prev" and "next" btns are now working successfully on their corresponding traversal through episodes!

--- react-vite/src/components/EpisodePage/EpisodePage.jsx
+++ react-vite/src/components/EpisodePage/EpisodePage.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import { FaEdit } from 'react-icons/fa';
+import { MdDeleteForever } from 'react-icons/md';
 import './EpisodePage.css'
 
 function EpisodePage() {
@@ -21,9 +22,11 @@ function EpisodePage() {
                 <div className='episodePageTop'>
                     <NavLink to={`/anime/${animeId}`}>Anime</NavLink>
                     <h1>Episode {episode.episodeNum}: {episode.title}</h1>
-                    <button><FaEdit /></button>
+                    <div className='ep-edit-delete'>
+                        <button><FaEdit /></button>
+                        <button><MdDeleteForever /></button>
+                    </div>
                 </div>
-
                 <div className='ep-plot-img-container'>
                     <div className='ep-plot-img'>
                         <img src={episode.previewImage} alt={episode.title} />
@@ -31,12 +34,11 @@ function EpisodePage() {
                         {episode.plot}
                     </div>
                 </div>
-
                 <div className='ep-prev-next-btns'>
                     {currentEpisodeIndex > 0 ? 
-                        <button>prev</button> : <div></div>}
+                        <button onClick={() => navigate(`/anime/${animeId}/episode/${episodeList[currentEpisodeIndex-1].id}`)}>prev</button> : <div></div>}
                     {currentEpisodeIndex < episodeList.length - 1 ?
-                        <button>next</button> : <div></div>}
+                        <button onClick={() => navigate(`/anime/${animeId}/episode/${episodeList[currentEpisodeIndex+1].id}`)}>next</button> : <div></div>}
                     
                 </div>
             </div>
