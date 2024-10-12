2d06527bac842d14236617c9aa07a59990e7bc71
Daniel Ho <daniel.ho337@gmail.com>
2024-10-11 23:34:15 -0700

    Got rid of the unused useState in EpisodePage

--- react-vite/src/components/EpisodePage/EpisodePage.jsx
+++ react-vite/src/components/EpisodePage/EpisodePage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import { FaArrowLeft, FaEdit } from 'react-icons/fa';
