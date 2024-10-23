63cc2c2c534ce673e44d3668d8a3c100641c05ce
Daniel Ho <daniel.ho337@gmail.com>
2024-10-22 20:45:27 -0700

    Created the thunk for character catalog

--- react-vite/src/redux/character.js
+++ react-vite/src/redux/character.js
@@ -2,8 +2,28 @@ import { thunkAuthenticate } from "./session";
 
 
 //! Action
+const CHARACTER_CATALOG = 'character/characterCatalog'
 const NEW_CHARACTER = 'character/newCharacter'
 
 
 //! Action Creator
-const newCharacter = ()
+const characterCatalog = (characterCatalog) => ({
+    type: CHARACTER_CATALOG,
+    payload: characterCatalog
+})
+
+
+//! Thunk Action
+export const thunkCharacterCatalog = () => async (dispatch) => {
+    const response = await fetch("/api/character/catalog");
+    if (response.ok) {
+        const data = await response.json()
+        if (data.errors) {
+            return;
+        }
+        dispatch(characterCatalog(data))
+    }
+}
+
+
+// function characterReducer(state={  })
