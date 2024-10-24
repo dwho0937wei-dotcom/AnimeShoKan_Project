c3568f3feef00d76ee4886d5a6efc217d86d4eac
Daniel Ho <daniel.ho337@gmail.com>
2024-10-24 14:23:18 -0700

    Add the favicon image into the Anime and Character Catalog Pages.
    Limit the Character Preview Image to be 800px at max-width in the CharacterPage.

--- react-vite/src/components/AnimeCatalogPage/AnimeCatalogPage.jsx
+++ react-vite/src/components/AnimeCatalogPage/AnimeCatalogPage.jsx
@@ -9,6 +9,7 @@ function AnimeCatalogPage() {
     return (
         <>
             <div className='welcome'>
+                <img src="https://animeshokanbucket.s3.us-east-2.amazonaws.com/06473b3210e1457b9f8a1a331cd1a684.png" alt="AnimeShoKan" />
                 <h1>Welcome to the AnimeShoKan!</h1>
                 <div id="navCatalogs">
                     <NavLink className="navCatalog" to="/anime"><h1>Anime</h1></NavLink>
