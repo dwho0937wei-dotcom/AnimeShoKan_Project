a18a28c2a833d74f35f0f700b4bd7dcfd57e5c0b
Daniel Ho <daniel.ho337@gmail.com>
2024-12-14 19:29:25 -0800

    Created a route for adding a character to anime!

--- app/api/anime_routes.py
+++ app/api/anime_routes.py
@@ -4,9 +4,21 @@ from app.api.s3_helper import (
     upload_file_to_s3, get_unique_filename, remove_file_from_s3
 )
 from app.forms import AnimeForm, AnimeUpdateForm, EpisodeForm, EpisodeUpdateForm
-from app.models import Anime, db, Episode
+from app.models import Anime, anime_character_table, db, Episode
 
 anime_routes = Blueprint('anime', __name__)
+
+
+#! Work In Progress
+@anime_routes.route('/<int:animeId>/character/<int:characterId>')
+def addCharacterToAnime(animeId, characterId):
+    new_association = anime_character_table.insert().values(
+        animeId,
+        characterId,
+        characterType='minor'
+    )
+    db.session.execute(new_association)
+    db.session.commit()
 
 
 # @anime_routes.route('')
