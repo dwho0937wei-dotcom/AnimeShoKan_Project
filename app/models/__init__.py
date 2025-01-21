126b58cefebcf7d19be3d5532003f108e44cb870
Daniel Ho <daniel.ho337@gmail.com>
2025-01-21 10:32:46 -0800

    Migrated the "genres" table and add the model Genre into the __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,6 @@ from .db import db, environment, SCHEMA
 from .anime import Anime
 from .character import Character
 from .episode import Episode
+from .genre import Genre
 from .join_tables import anime_characters_table
 from .user import User
